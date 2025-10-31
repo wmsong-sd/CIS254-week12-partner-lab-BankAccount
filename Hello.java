99a71b27636c40a7b9411540b315672122b0fc06
wmsong-sd <songw1@my.smccd.edu>
2025-10-31 15:49:11 -0700

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-/** Description of program: This program prints a hello message on the screen
-   @author Wonmin
-   @since 8.25.2025
-*/
-public class Hello {
-    public static void main(String args[]) {
-        System.out.println("Hello, World! My name is " + args [0] + "\nWhat is your name?") ; 
-    }
-}
